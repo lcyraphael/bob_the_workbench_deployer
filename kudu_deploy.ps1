7893c025252a381f69314f91172d3018be50198a
Raphael Leung <lcyraphael@gmail.com>
2017-03-29 18:18:40 +0100

    Powershell script using Kudu API (to upload workbench .war file)

---
+++ kudu_deploy.ps1
@@ -0,0 +1,4 @@
+﻿$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
+$userAgent = "powershell/1.0"
+$apiUrl = "https://" + $websiteName + ".scm.azurewebsites.net/api/zip/site/wwwroot/webapps/ROOT/"
+Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method PUT -InFile $workbench 
