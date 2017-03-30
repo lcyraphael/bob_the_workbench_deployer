bcdb5b5607e8b9d9eb9723fe71da809ea3498cdb
Raphael Leung <lcyraphael@gmail.com>
2017-03-30 15:00:34 +0100

    Test variable passing

--- kudu_deploy.ps1
+++ kudu_deploy.ps1
@@ -1,4 +1,5 @@
 ﻿$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
 $userAgent = "powershell/1.0"
+$websiteName = "raphael000asdfg"
 $apiUrl = "https://" + $websiteName + ".scm.azurewebsites.net/api/zip/site/wwwroot/webapps/ROOT/"
 Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method PUT -InFile $workbench 
