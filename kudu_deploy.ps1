3d91b81f642efd5fc2f4941a53d0b77bd30f2395
Raphael Leung <lcyraphael@gmail.com>
2017-03-30 18:06:36 +0100

    Fix VFS URI

--- kudu_deploy.ps1
+++ kudu_deploy.ps1
@@ -8,7 +8,7 @@
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
 $userAgent = "powershell/1.0"
 $apiUrlVfs1 = "https://" + $websiteName + ".scm.azurewebsites.net/api/vfs/site/wwwroot/webapps/ROOT/index.jsp"
-$apiUrlVfs1 = "https://" + $websiteName + ".scm.azurewebsites.net/api/vfs/site/wwwroot/webapps/ROOT/background.png"
+$apiUrlVfs2 = "https://" + $websiteName + ".scm.azurewebsites.net/api/vfs/site/wwwroot/webapps/ROOT/background.png"
 $apiUrlZip = "https://" + $websiteName + ".scm.azurewebsites.net/api/zip/site/wwwroot/webapps/ROOT/"
 
 $headers = @{
