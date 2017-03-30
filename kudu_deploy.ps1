9d18994bfcdb8a7ed96bdb43fd4abeabcc422472
Raphael Leung <lcyraphael@gmail.com>
2017-03-30 17:55:13 +0100

    If-match in header

--- kudu_deploy.ps1
+++ kudu_deploy.ps1
@@ -9,5 +9,10 @@ $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0
 $userAgent = "powershell/1.0"
 $apiUrlVfs = "https://" + $websiteName + ".scm.azurewebsites.net/api/vfs/site/wwwroot/webapps/ROOT/index.jsp"
 $apiUrlZip = "https://" + $websiteName + ".scm.azurewebsites.net/api/zip/site/wwwroot/webapps/ROOT/"
-Invoke-RestMethod -Uri $apiUrlVfs -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method DELETE 
-Invoke-RestMethod -Uri $apiUrlZip -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method PUT -InFile $workbench_war
+
+$headers = @{
+    "Authorization" = ("Basic {0}" -f $base64AuthInfo)
+    "If-Match"      = "*"
+}
+Invoke-RestMethod -Uri $apiUrlVfs -Headers $headers -UserAgent $userAgent -Method DELETE 
+Invoke-RestMethod -Uri $apiUrlZip -Headers $headers -UserAgent $userAgent -Method PUT -InFile $workbench_war
