59b06534f70a7558fe37a07612ab23068bf9490c
Raphael Leung <lcyraphael@gmail.com>
2017-03-30 15:08:58 +0100

    Add env for variables

--- kudu_deploy.ps1
+++ kudu_deploy.ps1
@@ -1,5 +1,4 @@
-﻿$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
+﻿$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $env:username,$env:password)))
 $userAgent = "powershell/1.0"
-$websiteName = "raphael000asdfg"
-$apiUrl = "https://" + $websiteName + ".scm.azurewebsites.net/api/zip/site/wwwroot/webapps/ROOT/"
-Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method PUT -InFile $workbench 
+$apiUrl = "https://" + $env:websiteName + ".scm.azurewebsites.net/api/zip/site/wwwroot/webapps/ROOT/"
+Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method PUT -InFile $env:workbench 
