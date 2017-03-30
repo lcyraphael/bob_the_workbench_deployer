97271f75b9b52a1a27b65ba96f4b1998304902dc
Raphael Leung <lcyraphael@gmail.com>
2017-03-30 17:58:05 +0100

    Clear ROOT directory for .war

--- kudu_deploy.ps1
+++ kudu_deploy.ps1
@@ -7,12 +7,14 @@
 
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
 $userAgent = "powershell/1.0"
-$apiUrlVfs = "https://" + $websiteName + ".scm.azurewebsites.net/api/vfs/site/wwwroot/webapps/ROOT/index.jsp"
+$apiUrlVfs1 = "https://" + $websiteName + ".scm.azurewebsites.net/api/vfs/site/wwwroot/webapps/ROOT/index.jsp"
+$apiUrlVfs1 = "https://" + $websiteName + ".scm.azurewebsites.net/api/vfs/site/wwwroot/webapps/ROOT/background.png"
 $apiUrlZip = "https://" + $websiteName + ".scm.azurewebsites.net/api/zip/site/wwwroot/webapps/ROOT/"
 
 $headers = @{
     "Authorization" = ("Basic {0}" -f $base64AuthInfo)
     "If-Match"      = "*"
 }
-Invoke-RestMethod -Uri $apiUrlVfs -Headers $headers -UserAgent $userAgent -Method DELETE 
+Invoke-RestMethod -Uri $apiUrlVfs1 -Headers $headers -UserAgent $userAgent -Method DELETE 
+Invoke-RestMethod -Uri $apiUrlVfs2 -Headers $headers -UserAgent $userAgent -Method DELETE 
 Invoke-RestMethod -Uri $apiUrlZip -Headers $headers -UserAgent $userAgent -Method PUT -InFile $workbench_war
